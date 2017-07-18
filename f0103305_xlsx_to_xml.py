188168507931a942bfc11b772b5c881159e77c24
Victor Gavro <vgavro@gmail.com>
2017-07-18 18:24:30 +0300

    f0103305: changed filename

--- f0103305_xlsx_to_xml.py
+++ f0103305_xlsx_to_xml.py
@@ -8,7 +8,10 @@ from collections import OrderedDict
 import xlrd
 
 
-FILENAME_TEMPLATE = '{C_STI_ORIG}{TIN}F010330510000000012032017{C_STI_ORIG}.xml'
+FILENAME_TEMPLATE = ('{C_STI_ORIG}{TIN}'
+                     '{C_DOC}{C_DOC_SUB}{C_DOC_VER:02d}'
+                     '1{PERIOD_TYPE}{PERIOD_MONTH:02d}{PERIOD_YEAR}'
+                     '{C_STI_ORIG}.xml')
 
 DECLARHEAD_FIELDS = ('TIN,C_DOC,C_DOC_SUB,C_DOC_VER,C_DOC_TYPE,C_DOC_CNT,'
                      'C_REG,C_RAJ,PERIOD_MONTH,PERIOD_TYPE,PERIOD_YEAR,'
@@ -27,7 +30,7 @@ DEFAULTS = OrderedDict([
 ])
 
 
-def create_xml(data):
+def create_xml(data, output_dir='./', encoding='windows-1251'):
     root = ET.Element('DECLAR', {'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
                                  'xsi:noNamespaceSchemaLocation': 'F0103305.xsd'})
     head = ET.SubElement(root, 'DECLARHEAD')
@@ -35,6 +38,7 @@ def create_xml(data):
 
     data_ = DEFAULTS.copy()
     data_.update(data)
+    data_['PERIOD_MONTH'] = int(data_['PERIOD_MONTH'])  # for proper filename formatting
 
     for key, value in data_.items():
         if callable(value):
@@ -42,6 +46,7 @@ def create_xml(data):
 
         if not value and key not in ['C_DOC_TYPE', 'HNACTL']:
             continue
+
         if isinstance(value, float):
             value = '{:.2f}'.format(value)
 
@@ -61,13 +66,10 @@ def create_xml(data):
 
     ET.SubElement(head, 'LINKED_DOCS', {'xsi:nil': 'true'})
     ET.SubElement(head, 'SOFTWARE', {'xsi:nil': 'true'})
-    return ET.ElementTree(root)
 
-
-def write_xml(data, output_dir='./', encoding='windows-1251'):
-    filename = os.path.join(output_dir, FILENAME_TEMPLATE.format(**data))
-    print('Creating {}'.format(filename))
-    create_xml(data).write(filename, encoding)
+    filename = os.path.join(output_dir, FILENAME_TEMPLATE.format(**data_))
+    ET.ElementTree(root).write(filename, encoding)
+    print('Created {}'.format(filename))
 
 
 def main(xlsx_filename='Книга1.xlsx', sheet_index=0,
@@ -92,7 +94,7 @@ def main(xlsx_filename='Книга1.xlsx', sheet_index=0,
     for i in range(data_start_row_index, sheet.nrows):
         data = OrderedDict(zip(fields, map(parse_value, sheet.row_values(i))))
         try:
-            write_xml(data, output_dir)
+            create_xml(data, output_dir)
         except Exception as exc:
             if not supress_exc:
                 raise
@@ -111,5 +113,5 @@ if __name__ == '__main__':
             else:
                 main(filename)
     except Exception as e:
-        print(e)
+        print('Error', repr(e))
     input('DONE. press any key to close')
